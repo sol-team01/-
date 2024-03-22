e5a4c7dbfb77b2baa0dd937cb937bb6b9e127be3
jieun <qwq12q12@gmail.com>
2024-03-22 12:36:11 +0900

    0.0.1v - 패키지 명 프로젝트로 변경

---
+++ 0.0.0v/ProjectA/src/main/java/com/hac/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.hac.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/");
+    }
+}
