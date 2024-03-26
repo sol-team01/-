4be63cd4072eaa63dcde0d268b9b04179dfab6c4
imPark <cuter6124@naver.com>
2024-03-26 16:31:49 +0900

    LoginMapper.xml 작성 완.

---
+++ 0.0.1v/HealthACare/src/main/java/com/hac/dto/searchDto/LoginDto.java
@@ -0,0 +1,11 @@
+package com.hac.dto.searchDto;
+
+import lombok.Data;
+
+@Data 
+public class LoginDto {
+	
+	private String U_id = "";
+	private String U_pw = "";
+	
+}
---
+++ 0.0.1v/HealthACare/src/main/java/com/hac/dto/userDto/InfoDto.java
@@ -0,0 +1,18 @@
+package com.hac.dto.userDto;
+
+import lombok.Data;
+
+@Data
+public class InfoDto {
+
+	private String I_no;
+	private String U_no;
+	private String I_name;
+	private String I_weight;
+	private String I_height;
+	private String I_email;
+	private String I_pwFinding;
+	private String I_createdAt;
+	private String I_updatedAt;
+	
+}
