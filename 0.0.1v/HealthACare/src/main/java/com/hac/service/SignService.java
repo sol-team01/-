f0aa5af2ebec0d77bf7ffec839e576b577e18497
kwon <kwon.com>
2024-03-27 14:11:59 +0900

    기본 서비스 등록

--- 0.0.1v/HealthACare/src/main/java/com/hac/service/SignService.java
+++ 0.0.1v/HealthACare/src/main/java/com/hac/service/SignService.java
@@ -1,5 +1,13 @@
 package com.hac.service;
 
-public interface SignService {
+import java.util.ArrayList;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.hac.dto.searchDto.LoginDto;
+
+public interface SignService {
+	public void login(LoginDto dto);
+	public ArrayList<LoginDto> LoginList();
+	public LoginDto signIn(@Param("id") String id, @Param("pw") String pw);
 }
