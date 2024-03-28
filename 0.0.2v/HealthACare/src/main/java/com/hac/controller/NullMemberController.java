f9101f9159cfac2486d55bb6fd5a1cb08b83d8f1
jieun <qwq12q12@gmail.com>
2024-03-28 10:24:35 +0900

    위대한 뒤로가기 도전

--- 0.0.2v/HealthACare/src/main/java/com/hac/controller/NullMemberController.java
+++ 0.0.2v/HealthACare/src/main/java/com/hac/controller/NullMemberController.java
@@ -1,13 +1,10 @@
 package com.hac.controller;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.hac.service.N_BbsService;
+import com.hac.service.BasicService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -16,16 +13,15 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/NullMember/*")
 @AllArgsConstructor	
 @Controller
-@Qualifier
 public class NullMemberController {
 	
-	private N_BbsService service;
+	private BasicService service;
 	
-	@GetMapping("/BbsFood")	
-	public String BbsCustomer(@RequestParam(value = "currentPage", defaultValue = "1") int currentPage, Model model) {
-		System.out.println("apicontroller로 대체됨");
-		return "redirect:/NullMember/test";
-	}
+//	@GetMapping("/BbsFood")	
+//	public String BbsCustomer(@RequestParam(value = "currentPage", defaultValue = "1") int currentPage, Model model) {
+//		System.out.println("apicontroller로 대체됨");
+//		return "redirect:/NullMember/test";
+//	}
 	@GetMapping("/BbsDisease")	
 	public String n_BbsDisease() {
 		System.out.println("컨트롤러진입");
@@ -37,7 +33,8 @@ public class NullMemberController {
 		return "NullMember/BbsHealth";
 	}
 	@GetMapping("/BbsCustomer")	
-	public void n_BbsCustomer(@RequestParam(value = "currentPage", defaultValue = "1") int currentPage, Model model) {
-    	model = service.getList(currentPage, model);
+	public String n_BbsCustomer() {
+		System.out.println("컨트롤러진입");
+		return "NullMember/BbsCustomer";
 	}
 }
