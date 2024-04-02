8edf07784ef59f70fd1f36955bbebad15c9a5d17
minsung <alarm153@naver.com>
2024-04-02 17:38:41 +0900

    자꾸 충돌오류 어케해결함

--- 0.0.3v/HealthACare/src/main/java/com/hac/service/BoardService.java
+++ 0.0.3v/HealthACare/src/main/java/com/hac/service/BoardService.java
@@ -10,16 +10,8 @@ public interface BoardService {
 	public ArrayList<BoardDto> getList(int currentPage);
 	public ArrayList<BoardSDto> searchList(int currentPage, String word, String column);
 	public int totalContent();
-<<<<<<< HEAD
 	public int searchTotalContent(BoardSDto dto);
 	public PagingProcessor pageBlock(int currentPage,BoardSDto dto);
-=======
-	public PagingProcessor pageBlock(int currentPage,String word);
-<<<<<<< HEAD
-
-=======
->>>>>>> 295020fb9567452ce94731ceba0882f84ee8711c
->>>>>>> d83c94dd0b35a7ec3b2ebc1ac9a25d9207798569
 	public BoardDto read(long bno);
 	public void write(BoardDto dto);
 	public void modify(BoardDto dto);
