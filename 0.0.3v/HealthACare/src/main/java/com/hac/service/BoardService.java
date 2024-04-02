9decb040d2502e30f466f8893b99c63f59e1ee86
jieun <qwq12q12@gmail.com>
2024-04-02 12:40:04 +0900

    연동용 푸쉬

--- 0.0.3v/HealthACare/src/main/java/com/hac/service/BoardService.java
+++ 0.0.3v/HealthACare/src/main/java/com/hac/service/BoardService.java
@@ -5,28 +5,14 @@ import java.util.ArrayList;
 import com.hac.dto.boardDto.BoardDto;
 import com.hac.dto.boardDto.BoardSDto;
 import com.hac.process.PagingProcessor;
-
 public interface BoardService {
 	public ArrayList<BoardDto> getList(int currentPage);
 	public ArrayList<BoardSDto> searchList(int currentPage, String word);
 	public int totalContent();
 	public PagingProcessor pageBlock(int currentPage,String word);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 29fc4aa72bd12b36257ef8cfd7ff285e1a8157f9
 	public BoardDto read(long bno);
 	public void write(BoardDto dto);
 	public void modify(BoardDto dto);
 	public void del(long bno);
 	public int hit(long bno);
-<<<<<<< HEAD
-=======
-
->>>>>>> 3721dc38d9a3621bccef62a819ef7b863136daa8
->>>>>>> adc40ecb0e103bdc166230867215e5a1b67730ba
-=======
->>>>>>> 29fc4aa72bd12b36257ef8cfd7ff285e1a8157f9
 }
