8c8ca731bfd76f965055fc1f98a4da11a3ffe21b
jieun <qwq12q12@gmail.com>
2024-04-02 09:20:32 +0900

    유튜부 api 연결

--- 0.0.3v/HealthACare/src/main/java/com/hac/controller/HomeController.java
+++ 0.0.3v/HealthACare/src/main/java/com/hac/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.hac.controller;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -29,27 +31,32 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 //		logger.info("Welcome home! The client locale is {}.", locale);
-//		
 //		Date date = new Date();
 //		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//		
 //		String formattedDate = dateFormat.format(date);
-//		
 //		model.addAttribute("serverTime", formattedDate );
 		
-		String API_KEY = "AIzaSyAJ3ySaW26tlc1QrySK__0YcATBgUolbWI";									
-		String API_BASIC = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=5&regionCode=kr&key="+API_KEY;									
-		log.info(API_BASIC);
-		RestTemplate restTemplate = new RestTemplate();									
-		URI uri_BASIC = null; //java.net.URI 임포트 하셈									
-		try {									
-			uri_BASIC = new URI(API_BASIC);								
-		} catch (URISyntaxException e) {									
-			e.printStackTrace();								
-		}			
-		String s = restTemplate.getForObject(uri_BASIC, String.class); 
-		log.info("api 테스트"+s);
-		
+		String API_KEY = "AIzaSyBDCffnIxE1dnQFMQwIWdZjVoU3VvPTDrQ";									
+		String API_Q = "운동";	
+		String API_URL=null;
+		try {
+		String encodedSearchId = URLEncoder.encode(API_Q, "UTF-8");
+		API_URL ="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q="+encodedSearchId+"&type=video&key="+API_KEY;
+		log.info(API_URL);
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+	RestTemplate restTemplate = new RestTemplate();									
+	
+	//// **** 중요 **** uri									
+	URI uri = null; //java.net.URI 임포트 하셈									
+	try {									
+		uri = new URI(API_URL);								
+	} catch (URISyntaxException e) {									
+		e.printStackTrace();								
+	}			
+	String s = restTemplate.getForObject(uri, String.class); 	
+	log.info(s);	
 		return "home";
 	}
 	
