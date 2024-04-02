3721dc38d9a3621bccef62a819ef7b863136daa8
jieun <qwq12q12@gmail.com>
2024-04-02 11:12:19 +0900

    home java 유튜브 api 할당량 사용 초과로 주석처리

--- 0.0.3v/HealthACare/src/main/java/com/hac/controller/HomeController.java
+++ 0.0.3v/HealthACare/src/main/java/com/hac/controller/HomeController.java
@@ -1,9 +1,5 @@
 package com.hac.controller;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URLEncoder;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -12,9 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.client.RestTemplate;
-
-import com.hac.dto.youtubeApi.YoutubeApiDto;
 
 import lombok.extern.log4j.Log4j;
 
@@ -38,36 +31,38 @@ public class HomeController {
 //		String formattedDate = dateFormat.format(date);
 //		model.addAttribute("serverTime", formattedDate );
 		
-		String API_KEY = "AIzaSyBDCffnIxE1dnQFMQwIWdZjVoU3VvPTDrQ";									
-		String API_Q = "운동";	
-		String API_URL=null;
-		try {
-		String encodedSearchId = URLEncoder.encode(API_Q, "UTF-8");
-		API_URL ="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q="+encodedSearchId+"&type=video&key="+API_KEY;
-		log.info(API_URL);
-		} catch (UnsupportedEncodingException e1) {
-			e1.printStackTrace();
-		}
-	RestTemplate restTemplate = new RestTemplate();									
-	
-	//// **** 중요 **** uri									
-	URI uri = null; //java.net.URI 임포트 하셈									
-	try {									
-		uri = new URI(API_URL);								
-	} catch (URISyntaxException e) {									
-		e.printStackTrace();								
-	}			
-	String s = restTemplate.getForObject(uri, String.class); 	
-	YoutubeApiDto youtube = restTemplate.getForObject(uri, YoutubeApiDto.class); // 자기 클래스로 바꾸시오..
-	model.addAttribute("YoutubeDtoBasic0", youtube.items.get(0).id.videoId);
-	model.addAttribute("YoutubeDtoBasic1", youtube.items.get(1).id.videoId);
-	model.addAttribute("YoutubeDtoBasic2", youtube.items.get(2).id.videoId);
-	model.addAttribute("YoutubeDtoBasic3", youtube.items.get(3).id.videoId);
-	model.addAttribute("YoutubeDtoBasic4", youtube.items.get(4).id.videoId);
-	model.addAttribute("YoutubeDtoBasic5", youtube.items.get(5).id.videoId);
-	log.info(youtube.items.get(5).id.videoId);
-	log.info(youtube.items.get(4).id.videoId);
-	log.info(youtube.items.get(3).id.videoId);
+		//(최) 유튜브 api 전송량이 적어서 오류가 자주 뜨기 때문에 주석처리 해놓음
+		// 정상작동은 합니다.
+//		String API_KEY = "AIzaSyBDCffnIxE1dnQFMQwIWdZjVoU3VvPTDrQ";									
+//		String API_Q = "운동";	
+//		String API_URL=null;
+//		try {
+//		String encodedSearchId = URLEncoder.encode(API_Q, "UTF-8");
+//		API_URL ="https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=6&q="+encodedSearchId+"&type=video&key="+API_KEY;
+//		log.info(API_URL);
+//		} catch (UnsupportedEncodingException e1) {
+//			e1.printStackTrace();
+//		}
+//	RestTemplate restTemplate = new RestTemplate();									
+//	
+//	//// **** 중요 **** uri									
+//	URI uri = null; //java.net.URI 임포트 하셈									
+//	try {									
+//		uri = new URI(API_URL);								
+//	} catch (URISyntaxException e) {									
+//		e.printStackTrace();								
+//	}			
+//	String s = restTemplate.getForObject(uri, String.class); 	
+//	YoutubeApiDto youtube = restTemplate.getForObject(uri, YoutubeApiDto.class); // 자기 클래스로 바꾸시오..
+//	model.addAttribute("YoutubeDtoBasic0", youtube.items.get(0).id.videoId);
+//	model.addAttribute("YoutubeDtoBasic1", youtube.items.get(1).id.videoId);
+//	model.addAttribute("YoutubeDtoBasic2", youtube.items.get(2).id.videoId);
+//	model.addAttribute("YoutubeDtoBasic3", youtube.items.get(3).id.videoId);
+//	model.addAttribute("YoutubeDtoBasic4", youtube.items.get(4).id.videoId);
+//	model.addAttribute("YoutubeDtoBasic5", youtube.items.get(5).id.videoId);
+//	log.info(youtube.items.get(5).id.videoId);
+//	log.info(youtube.items.get(4).id.videoId);
+//	log.info(youtube.items.get(3).id.videoId);
 	//	log.info(s);	
 		return "home";
 	}
