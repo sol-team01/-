1bdc572d87a9c90ce569ae8312098a1c47f9153f
jieun <qwq12q12@gmail.com>
2024-04-02 12:31:30 +0900

    home jsp 최적화

--- 0.0.3v/SQL.sql
+++ 0.0.3v/SQL.sql
@@ -55,3 +55,16 @@ SELECT YEAR(P_createdAt),MONTH(P_createdAt),AVG(P_weightLog) AS average_value_pe
 FROM physicalLog
 GROUP BY YEAR(P_createdAt), MONTH(P_createdAt)
 ORDER BY YEAR(P_createdAt), MONTH(P_createdAt);
+
+
+CREATE TABLE `board` (
+		`B_NO` INT PRIMARY KEY AUTO_INCREMENT, 			#글번호
+		`B_TITLE` CHAR(100) NOT NULL DEFAULT "",			#글제목
+	    `B_ID` CHAR(50) NOT NULL,							#작성자ID
+		`B_DATETIME` DATETIME NOT NULL DEFAULT now(),		#작성시간
+	    `B_HIT` INT NOT NULL DEFAULT 0,					#조회수    
+	    `B_TEXT` TEXT	NOT NULL,							#글내용, 댓글내용
+        `B_CATEGORY` TEXT NOT NULL,						#카테고리
+	    `B_REPLY_COUNT` INT NOT NULL DEFAULT 0,			#댓글수
+	    `B_REPLY_ORI` INT	NOT NULL DEFAULT -1				#댓글의 원글 번호
+	);
