1ff69dbfefd95f1669f84dea3a92f765f3636088
minsung <alarm153@naver.com>
2024-04-04 11:42:13 +0900

    head 삭제판 (감독버전)

--- 0.0.3v/HealthACare/src/main/java/com/hac/controller/SignController.java
+++ 0.0.3v/HealthACare/src/main/java/com/hac/controller/SignController.java
@@ -67,17 +67,7 @@ public class SignController {
 		HttpSession session = request.getSession();
 		System.out.println("=======로그인 잘 통과하는가?=======");
 		if(dto != null) {
-<<<<<<< HEAD
 			session.setAttribute("login", dto);
-			System.out.println(dto);
-=======
-			SignDto user = new SignDto();
-			user.setU_no(dto.getU_no());
-			user.setI_name(dto.getI_name());
-			user.setU_id(dto.getU_id());
-			session.setAttribute("U_no", dto.getU_no());
-			session.setAttribute("login", user);
->>>>>>> 41af2e85d843b3336ed1f58b6fdc39de21628f6a
 			System.out.println("로그인 성공");
 			return "redirect:/";
 		} else {
@@ -96,10 +86,6 @@ public class SignController {
 	public String logout(HttpServletRequest request) {
 	    HttpSession session = request.getSession();
 	    session.removeAttribute("login");
-<<<<<<< HEAD
-=======
-	    session.removeAttribute("U_no");
->>>>>>> 41af2e85d843b3336ed1f58b6fdc39de21628f6a
 	    return "redirect:/";
 	}
 	
@@ -162,10 +148,7 @@ public class SignController {
 	    
 	    return result;
 	}
-<<<<<<< HEAD
-=======
 	
-<<<<<<< HEAD
 	//비밀번호 찾기
 	@PostMapping("/searchPw")
 	@ResponseBody //Spring 프레임워크에서 컨트롤러 메소드가 HTTP 응답 본문을 직접 반환하도록 지시하는 어노테이션.
@@ -181,13 +164,10 @@ public class SignController {
 		}
 		return null;
 	}
-=======
 //	//비밀번호 찾기
 //	@PostMapping
 //	@ResponseBody //Spring 프레임워크에서 컨트롤러 메소드가 HTTP 응답 본문을 직접 반환하도록 지시하는 어노테이션.
 //	public String
->>>>>>> 074622aa370a11aff6a68502b35026d24ada6710
->>>>>>> 41af2e85d843b3336ed1f58b6fdc39de21628f6a
 
 	//아이디 찾기 jsp 진입
 	@GetMapping("/searchIdPw")
