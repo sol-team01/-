2d3bcb6dc2f3fae02293c20ef1ddae6503338d3a
imPark <cuter6124@naver.com>
2024-04-04 09:06:19 +0900

    식단 로그인버그 수정

--- 0.0.3v/HealthACare/src/main/java/com/hac/controller/FoodController.java
+++ 0.0.3v/HealthACare/src/main/java/com/hac/controller/FoodController.java
@@ -125,7 +125,7 @@ public class FoodController {
 			HttpSession session = request.getSession(false); // 세션이 없으면 새로 생성하지 않음
 
 		    // 세션에 로그인 정보가 없으면 로그인 페이지로 리다이렉트
-		    if (session == null || session.getAttribute("login") == null) {
+		    if (session == null || session.getAttribute("U_no") == null) {
 		    	
 		        return "redirect:/page/login";
 		    }
@@ -159,7 +159,7 @@ public class FoodController {
 			 return "/food/calorieCounting";
 		}
 		
-<<<<<<< HEAD
+
 		// /favicon.ico 경로에 대한 요청을 처리하는 핸들러 메서드
 	    @RequestMapping(value = "/favicon.ico", method = RequestMethod.GET)
 	    public void favicon(HttpServletResponse response) {
@@ -167,8 +167,5 @@ public class FoodController {
 	        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 	    }
 }
-=======
-    
-      
-}
->>>>>>> 518b0a07e3cbf3cef3d25a063b4180d767e39016
+
+
