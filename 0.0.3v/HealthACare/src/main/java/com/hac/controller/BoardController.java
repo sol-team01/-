580ebd352ff7a02ad2febc694719b7de5dd948b1
NamHee <gjeosld052@naver.com>
2024-04-05 17:19:18 +0900

    뭔지 모르는거 커밋

--- 0.0.3v/HealthACare/src/main/java/com/hac/controller/BoardController.java
+++ 0.0.3v/HealthACare/src/main/java/com/hac/controller/BoardController.java
@@ -3,7 +3,6 @@ package com.hac.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.apache.ibatis.reflection.SystemMetaObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
